a862f0f60694ac317e58c39b07965f7337e953d2
Aleksandr Gonchar <shuric2005@mail.ru>
2015-10-14 18:57:39 +0300

    add classdate.php

---
+++ ClassDate.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: device
+ * Date: 14.10.15
+ * Time: 15:17
+ */
+namespace lesson2;
+require 'vendor/autoload.php';
+
+use Carbon\Carbon;
+
+class MyDate extends Carbon
+{
+
+    public static function a2($day,$month,$year)
+    {
+        $yearnow=date("Y");
+        $nowday=Carbon::instance(new \DateTime());
+        $newyear=Carbon::instance(new \DateTime("01.01.$yearnow"));
+        if ($day==null || $month==null || $year==null) {
+            $day=date("d");
+            $month=date("m");
+            $year=date("Y");
+        }
+        $enterdate=Carbon::instance(new \DateTime("$day.$month.$year"));
+        $diffday=($enterdate->day)-($nowday->day);
+        $diffmonth=($enterdate->month)-($nowday->month);
+        $diffyear=($enterdate->year)-($nowday->year);
+        $daystartyear=$nowday->dayOfYear;
+        $monthstartyear=($nowday->month)-($newyear->month);
+        $timezons=$nowday->timezoneName;
+        $mes='You entered '. $enterdate->toDateString().'<br/>';
+        $mes.='Now '.$nowday->toDateString().'<br/>';
+        if ($diffday==0 && $diffmonth==0) {
+            $mes.='You enter the current date';
+        } elseif ($diffday>0 && $diffmonth>0) {
+            $mes.='More than the current date entered in the '.$diffday.' days and '.$diffmonth.' month '.$diffyear.' year';
+        } else {
+            $mes.='Less than the current date entered by '.abs($diffday).' days and '.abs($diffmonth).' month '.abs($diffyear).' year';
+        }
+        $mes.="<br/> Your time zone: $timezons";
+        $mes.="<br/> Since the beginning of this year went $monthstartyear full months or $daystartyear days";
+        return  $mes;
+    }
+}
